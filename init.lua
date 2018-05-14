ff9dbd5ed48d23dad6cbba7bc0b0f46173cdc9ff
Thomas Rudin <thomas@rudin.io>
2018-05-14 11:04:31 +0200

    working vacuum damage

--- init.lua
+++ init.lua
@@ -7,9 +7,11 @@ vacuum = {
 local MP = minetest.get_modpath("vacuum")
 
 dofile(MP.."/vacuum.lua")
+dofile(MP.."/airbottle.lua")
 dofile(MP.."/airpump.lua")
 dofile(MP.."/mapgen.lua")
 dofile(MP.."/abm.lua")
+dofile(MP.."/functions.lua")
 dofile(MP.."/dignode.lua")
 
 print("[OK] Vacuum")
