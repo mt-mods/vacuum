25ac83388f2d88ac49b66a1e1cd0bfe498bbd753
Thomas Rudin <thomas@rudin.io>
2018-06-07 08:32:15 +0200

    digtron move cycle compat

--- dignode.lua
+++ dignode.lua
@@ -1,6 +1,11 @@
 
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
+	if oldnode.name and string.sub(oldnode.name, 0, 7) == "digtron" then
+		-- skip digtron moved nodes
+		return;
+	end
+
 	local np = minetest.find_node_near(pos, 1,{"vacuum:vacuum"})
 	if np ~= nil then
 		minetest.set_node(pos, {name = "vacuum:vacuum"})
