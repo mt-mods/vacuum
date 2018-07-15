fb9920c1caff3359cf87a0706175b7159f2128d4
Thomas Rudin <thomas@rudin.io>
2018-07-15 22:28:39 +0200

    working air bottles

--- airpump.lua
+++ airpump.lua
@@ -2,8 +2,24 @@ local has_technic_mod = minetest.get_modpath("technic")
 
 -- flush room with air
 vacuum.flush_air = function(pos, i)
+
+	local meta = minetest.get_meta(pos)
+	local inv = meta:get_inventory()
+
+	local air_bottle_present = inv:contains_item("main", {name="vacuum:air_bottle", count=1})
+	if air_bottle_present then
+		local new_stack = ItemStack("vessels:steel_bottle")
+
+		if inv:room_for_item("main", new_stack) then
+			inv:remove_item("main", {name="vacuum:air_bottle", count=1})
+			inv:add_item("main", new_stack)
+		end
+	else
+		meta:set_int("powerstorage", 0)
+	end
+
 	if i == nil then
-		-- default to 7 layers
+		-- default layers
 		i = vacuum.air_pump_range
 	end
 
@@ -39,6 +55,14 @@ local is_airpump_ready = function(meta)
 		return true
 	end
 
+	local inv = meta:get_inventory()
+
+	local air_bottle_present = inv:contains_item("main", {name="vacuum:air_bottle", count=1})
+
+	if air_bottle_present then
+		return true
+	end
+
 	-- check power levels
 	local store = meta:get_int("powerstorage")
 	return store >= vacuum.air_pump_power_store
@@ -61,9 +85,11 @@ end
 
 -- update airpump formspec
 local update_formspec = function(meta)
-	meta:set_string("formspec", "size[8,2;]" ..
+	meta:set_string("formspec", "size[8,7.2;]" ..
 		"label[0,0;Flush]" ..
 		"button_exit[0,1;8,1;flush;Flush]" ..
+		"list[context;main;0,2;8,1;]" ..
+		"list[current_player;main;0,3.2;8,4;]" ..
 		"")
 
 	update_infotext(meta)
@@ -85,16 +111,23 @@ minetest.register_node("vacuum:airpump", {
 		action_on = function (pos, node)
 			local meta = minetest.get_meta(pos)
 			if is_airpump_ready(meta) then
-				meta:set_int("powerstorage", 0)
 				vacuum.flush_air(pos)
 				update_infotext(meta)
 			end
 		end
 	}},
 
+	after_place_node = function(pos, placer)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("owner", placer:get_player_name() or "")
+	end,
+
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		meta:set_int("enabled", 0)
+
+		local inv = meta:get_inventory()
+		inv:set_size("main", 8)
 
 		if has_technic_mod then
 			meta:set_int("HV_EU_input", 0)
@@ -104,11 +137,16 @@ minetest.register_node("vacuum:airpump", {
 		update_formspec(meta)
 	end,
 
+	can_dig = function(pos,player)
+		local meta = minetest.get_meta(pos);
+		local inv = meta:get_inventory()
+		return inv:is_empty("main")
+	end,
+
 	on_receive_fields = function(pos, formname, fields, sender)
 		local meta = minetest.get_meta(pos);
 
 		if fields.flush and is_airpump_ready(meta) then
-			meta:set_int("powerstorage", 0)
 			vacuum.flush_air(pos)
 		end
 
@@ -118,12 +156,15 @@ minetest.register_node("vacuum:airpump", {
 
 	technic_run = function(pos, node)
 		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
 
 		local eu_input = meta:get_int("HV_EU_input")
 		local demand = meta:get_int("HV_EU_demand")
 		local store = meta:get_int("powerstorage")
 
-		if store < vacuum.air_pump_power_store then
+		local empty_vessel_present = inv:contains_item("main", {name="vessels:steel_bottle", count=1}) and pos.y <= vacuum.space_height
+
+		if store < vacuum.air_pump_power_store or empty_vessel_present then
 			-- not full, charge
 			meta:set_int("HV_EU_demand", vacuum.air_pump_power_draw)
 		else
@@ -131,7 +172,16 @@ minetest.register_node("vacuum:airpump", {
 			meta:set_int("HV_EU_demand", 0)
 		end
 
-		if eu_input >= 0 then
+		if empty_vessel_present then
+			-- on earth and empty steel bottle present
+			local new_stack = ItemStack("vacuum:air_bottle")
+
+			if inv:room_for_item("main", new_stack) then
+				inv:remove_item("main", {name="vessels:steel_bottle", count=1})
+				inv:add_item("main", new_stack)
+			end
+
+		elseif eu_input >= 0 then
 			-- power!
 			meta:set_int("powerstorage", meta:get_int("powerstorage") + eu_input)
 		end
