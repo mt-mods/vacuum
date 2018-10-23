701c599c66f5fd2e9d6a982cf1face6104abc2c7
Thomas Rudin <thomas@rudin.io>
2018-10-23 08:52:15 +0200

    fix #8

--- airbottle.lua
+++ airbottle.lua
@@ -6,11 +6,3 @@ minetest.register_craftitem("vacuum:air_bottle", {
 	inventory_image = vacuum.air_bottle_image
 })
 
-minetest.register_craft({
-	output = "vacuum:air_bottle",
-	recipe = {
-		{"vessels:steel_bottle", "vessels:steel_bottle", "vessels:steel_bottle"},
-		{"", "wool:blue", ""},
-		{"", "", ""}
-	}
-})
