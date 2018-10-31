252e4e879c20bbf5d714c0155fa62fd92067e364
Thomas Rudin <thomas@rudin.io>
2018-10-31 14:05:31 +0100

    explicit dirt nodes (desert sand excluded)

--- physics.lua
+++ physics.lua
@@ -88,10 +88,18 @@ minetest.register_abm({
 	end
 })
 
--- soil in vacuum
+-- various dirts in vacuum
 minetest.register_abm({
         label = "space vacuum soil dry",
-	nodenames = {"group:soil"},
+	nodenames = {
+		"default:dirt",
+		"default:dirt_with_grass",
+		"default:dirt_with_snow",
+		"default:dirt_with_dry_grass",
+		"default:dirt_with_grass_footsteps",
+		"default:dirt_with_rainforest_litter",
+		"default:dirt_with_coniferous_litter"
+	},
 	neighbors = {"vacuum:vacuum"},
 	interval = 1,
 	chance = 2,
