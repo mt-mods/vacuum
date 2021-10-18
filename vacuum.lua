834b8fd5c988fcc55bb520d279f47e5bec8fb31f
OgelGames <olliverdc28@gmail.com>
2021-10-19 00:14:14 +1100

    add vacuum inventory/wield texture

--- vacuum.lua
+++ vacuum.lua
@@ -10,10 +10,12 @@ minetest.register_node("vacuum:vacuum", {
 	drowning = 1,
 	post_effect_color = {a = 20, r = 20, g = 20, b = 250},
 	tiles = {"vacuum_texture.png^[colorize:#E0E0E033"},
+	inventory_image = "vacuum_inv.png",
+	wield_image = "vacuum_inv.png",
 	alpha = 0.1,
 	groups = {not_in_creative_inventory=1, not_blocking_trains=1, cools_lava=1},
 	paramtype = "light",
-	light_source = minetest.LIGHT_MAX,-- TOOD: test
+	light_source = minetest.LIGHT_MAX,
 	drop = {},
 	sunlight_propagates = true
 })
