d12caeb5c5eda04e21c44a87c81b8e3f9bc06036
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-01-29 08:09:11 +0100

    vacuum propagation rework

--- common.lua
+++ common.lua
@@ -3,12 +3,12 @@
 vacuum.air_bottle_image = "vessels_steel_bottle.png^[colorize:#0000FFAA"
 
 -- space pos checker
-vacuum.is_pos_in_space = function(pos)
+function vacuum.is_pos_in_space(pos)
 	return pos.y > vacuum.space_height
 end
 
 -- (cheaper) space check, gets called more often than `is_pos_in_space`
-vacuum.no_vacuum_abm = function(pos)
+function vacuum.no_vacuum_abm(pos)
 	return pos.y > vacuum.space_height - 40 and pos.y < vacuum.space_height + 40
 end
 
--- physics_propagation.lua
+++ physics_propagation.lua
@@ -1,3 +1,4 @@
+--[[
 local has_monitoring = minetest.get_modpath("monitoring")
 
 local metric_space_vacuum_abm
@@ -44,3 +45,68 @@ minetest.register_abm({
 		end
 	end)
 })
+--]]
+
+minetest.register_chatcommand("test", {
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		local pos = player:get_pos()
+		local radius = 20
+
+		local t0 = minetest.get_us_time()
+		vacuum.propagate_mapblock(pos, radius)
+		local t1 = minetest.get_us_time()
+		local us = t1 - t0
+		return true, "Propagation with radius of " .. radius .. " took " .. us .. " us"
+	end
+})
+
+local c_air = minetest.get_content_id("air")
+local c_vacuum = minetest.get_content_id("vacuum:vacuum")
+
+local directions = {
+	{ x=1, y=0, z=0 },
+	{ x=-1, y=0, z=0 },
+	{ x=0, y=1, z=0 },
+	{ x=0, y=-1, z=0 },
+	{ x=0, y=0, z=1 },
+	{ x=0, y=0, z=-1 },
+}
+
+function vacuum.propagate_mapblock(center_pos, radius)
+	local pos1 = vector.subtract(center_pos, radius)
+	local pos2 = vector.add(center_pos, radius)
+
+	-- inner positions (1 node margin)
+	local ipos1 = vector.add(pos1, 1)
+	local ipos2 = vector.subtract(pos2, 1)
+
+	local manip = minetest.get_voxel_manip()
+	local e1, e2 = manip:read_from_map(pos1, pos2)
+	local area = VoxelArea:new({MinEdge=e1, MaxEdge=e2})
+	local node_ids = manip:get_data()
+
+	for z=ipos1.z, ipos2.z do
+	for y=ipos1.y, ipos2.y do
+	for x=ipos1.x, ipos2.x do
+		local pos = { x=x, y=y, z=z }
+		local index = area:indexp(pos)
+		if node_ids[index] == c_air then
+			for _, dir in ipairs(directions) do
+				-- other neighboring pos
+				local opos = vector.add(pos, dir)
+				local oindex = area:indexp(opos)
+				if node_ids[oindex] == c_vacuum then
+					-- propagate
+					node_ids[index] = c_vacuum
+					break
+				end
+			end
+		end
+	end
+	end
+	end
+
+	manip:set_data(node_ids)
+	manip:write_to_map()
+end
